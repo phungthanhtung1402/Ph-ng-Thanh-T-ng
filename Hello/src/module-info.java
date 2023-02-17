a4826eed7d1da429a1fad9e8cabe84c0cc2d8f17
phungthanhtung1402 <125629157+phungthanhtung1402@users.noreply.github.com>
2023-02-17 11:12:57 +0700

    Add files via upload

---
+++ Hello/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author KyThuat88
+ *
+ */
+module Hello {
+}
---
+++ PhungThanhTung_Project/src/main/PhungThanhTung_App.java
@@ -0,0 +1,10 @@
+package main;
+
+public class PhungThanhTung_App 
+{
+	public static void main(String[] args)
+    {
+    	System.out.println("Phùng Thanh Tùng hoc Java"); 
+    }
+
+}
